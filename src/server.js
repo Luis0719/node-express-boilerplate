a7d2d0fa565a0479fb78ea3b638e44a80a054e31
Luis Hernandez Ayala <luis.hdez97@hotmail.com>
2020-09-05 19:32:59 -0600

    Add eslint

--- src/server.js
+++ src/server.js
@@ -1,23 +1,24 @@
 const express = require('express');
-const app = express();
 
+const app = express();
+// eslint-disable-next-line import/no-unresolved
 const { server: config } = require('config');
 
 // parse incoming json data
 app.use(express.json());
 app.use(express.urlencoded({
-    extended: true,
+  extended: true,
 }));
 
 // Register Routes
 require('./services')(app);
 
 // Create server
-app.listen(config.port, function () {
-    console.log(`Server listening on port ${config.port}`);
+app.listen(config.port, () => {
+  console.log(`Server listening on port ${config.port}`);
 });
 
 app.on('error', (error) => {
-    console.log("Server error");
-    throw error;
+  console.log('Server error');
+  throw error;
 });
