8ac202c3abd02e8eb0777f2107523f75c0ea8cf2
Luis Hernandez Ayala <luis.hdez97@hotmail.com>
2020-09-05 20:25:37 -0600

    fix preinstall order

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 // Loan environment variables
 require('dotenv').config({ path: `src/config/.env.${process.env.NODE_ENV}` });
 
-// Load custom dependencies
-require('../scripts/preinstall');
-
 // Create and load server
 require('./server');
