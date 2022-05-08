248d5d7e31ec921ad96b82370b7dace935a8311f
Luis Ayala <luis.hdza97@gmail.com>
2022-05-07 21:20:55 -0700

    add test init and teardown as async functions

--- test/init.js
+++ test/init.js
@@ -1,2 +1,8 @@
 const envVarsPath = `config/.env.${process.env.APP_ENV || "development"}`;
 require("dotenv").config({ path: envVarsPath });
+
+const dbUtils = require("./testCommon/database/dbUtils");
+
+module.exports = async () => {
+  await dbUtils.cleanDatabase();
+};
